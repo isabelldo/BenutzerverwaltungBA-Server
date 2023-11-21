7be7917ceca8b096764746ed1a05dbcb5ba0d4ee
isi <isabelldorendorf99@web.de>
2023-11-21 15:34:13 +0100

    added oauth2 logIn

--- src/main/java/io/swagger/configuration/HomeController.java
+++ src/main/java/io/swagger/configuration/HomeController.java
@@ -1,13 +1,22 @@
 package io.swagger.configuration;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Home redirection to swagger api documentation 
+ * Home redirection to swagger api documentation
  */
 @Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
+        return "index";
+    }
     @RequestMapping(value = "/")
     public String index() {
         return "redirect:/swagger-ui/";
